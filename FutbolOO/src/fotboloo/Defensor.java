f720a1f21c85cb26846d8b1374e4c1f2265c3178
Arnaldo Ocampo <arnaldo.ocampo@fiuni.edu.py>
2023-05-11 18:38:33 -0400

    Clase Defensor agregada

---
+++ FutbolOO/src/fotboloo/Defensor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fotboloo;
+
+/**
+ *
+ * @author Arnaldo
+ */
+public class Defensor extends Jugador{
+
+    @Override
+    public void festejar() {
+        System.out.println("No me celebran :(");   
+    }
+    
+}
